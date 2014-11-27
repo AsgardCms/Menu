c345f103a735966607c9f65cc0f99e684fc1729f
Nicolas Widart <n.widart@gmail.com>
2014-11-27 20:58:55 +0100

    Fix typo

--- Resources/lang/fr/messages.php
+++ Resources/lang/fr/messages.php
@@ -5,10 +5,10 @@ return [
     'menu created' => 'Menu créer.',
     'menu not found' => 'Menu non trouvé.',
     'menu updated' => 'Menu mit à jour.',
-    'menu deleted' => 'Menu supprimer',
+    'menu deleted' => 'Menu suprimer',
     /* MenuItem management */
     'menuitem created' => 'Elément de menu créer.',
     'menuitem not found' => 'Elément de menu non trouvé.',
     'menuitem updated' => 'Elément de menu mit à jour.',
-    'menuitem deleted' => 'Elément de menu supprimer',
+    'menuitem deleted' => 'Elément de menu suprimer',
 ];
