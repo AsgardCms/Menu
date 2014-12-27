0e70a9af6ad8943645d3b77a8aa1210022cbe499
Nicolas Widart <n.widart@gmail.com>
2014-12-27 12:03:20 +0100

    Remove the menu and menuitem listeners

--- listeners.php
+++ listeners.php
@@ -1,4 +1,1 @@
 <?php
-
-Event::listen('Modules.Menu.Events.MenuWasCreated', 'Modules\Menu\Listeners\RootMenuItemCreator');
-Event::listen('Modules.Menu.Events.MenuItemWasCreated', 'Modules\Menu\Listeners\MakeMenuItemChildOfRoot');
