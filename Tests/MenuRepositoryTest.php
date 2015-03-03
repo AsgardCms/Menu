f8087089e102574a16ae0ce9531fa5ce572e4e98
Nicolas Widart <n.widart@gmail.com>
2015-03-03 13:04:06 +0100

    Prepare a new test for checking root item creation

--- Tests/MenuRepositoryTest.php
+++ Tests/MenuRepositoryTest.php
@@ -10,4 +10,11 @@ class MenuRepositoryTest extends BaseMenuTest
         $this->assertEquals(1, $this->menu->find($menu->id)->count());
         $this->assertEquals($menu->name, $this->menu->find($menu->id)->name);
     }
+
+    public function it_should_create_root_item_when_creating_new_menu()
+    {
+        $menu = $this->createMenu('main', 'Main Menu');
+
+        $items = $this->menuItem->allRootsForMenu($menu->id);
+    }
 }
