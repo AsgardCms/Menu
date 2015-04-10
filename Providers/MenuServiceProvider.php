b09b8cfb30ec3a236046832aa1ac2777c504ecfb
Nicolas Widart <n.widart@gmail.com>
2015-04-10 08:37:46 +0200

    Register menus in the Service Provider

--- Providers/MenuServiceProvider.php
+++ Providers/MenuServiceProvider.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 use Modules\Menu\Entities\Menu;
+use Pingpong\Menus\Builder;
+use Pingpong\Menus\Facades\Menu as MenuFacade;
 use Modules\Menu\Entities\Menuitem;
+use Pingpong\Menus\MenuItem as PingpongMenuItem;
 use Modules\Menu\Repositories\Cache\CacheMenuDecorator;
 use Modules\Menu\Repositories\Cache\CacheMenuItemDecorator;
 use Modules\Menu\Repositories\Eloquent\EloquentMenuItemRepository;
@@ -26,6 +29,23 @@ class MenuServiceProvider extends ServiceProvider
     public function register()
     {
         $this->registerBindings();
+    }
+
+    /**
+     * Register all online menus on the Pingpong/Menu package
+     */
+    public function boot()
+    {
+        $menu = $this->app->make('Modules\Menu\Repositories\MenuRepository');
+        $menuItem = $this->app->make('Modules\Menu\Repositories\MenuItemRepository');
+        foreach ($menu->all() as $menu) {
+            $menuTree = $menuItem->getTreeForMenu($menu->id);
+            MenuFacade::create($menu->name, function (Builder $menu) use ($menuTree) {
+                foreach ($menuTree as $menuItem) {
+                    $this->addItemToMenu($menuItem, $menu);
+                }
+            });
+        }
     }
 
     /**
@@ -68,5 +88,65 @@ class MenuServiceProvider extends ServiceProvider
                 return new CacheMenuItemDecorator($repository);
             }
         );
+    }
+
+    /**
+     * Add a menu item to the menu
+     * @param Menuitem $item
+     * @param Builder $menu
+     */
+    public function addItemToMenu(Menuitem $item, Builder $menu)
+    {
+        if ($this->hasChildren($item)) {
+            $this->addChildrenToMenu($item->title, $item->items, $menu);
+        } else {
+            $target = $item->uri ?: $item->url;
+            $menu->url(
+                $target,
+                $item->title,
+                ['target' => $item->target]
+            );
+        }
+    }
+
+    /**
+     * Add children to menu under the give name
+     *
+     * @param string $name
+     * @param object $children
+     * @param Builder|MenuItem $menu
+     */
+    private function addChildrenToMenu($name, $children, $menu)
+    {
+        $menu->dropdown($name, function (PingpongMenuItem $subMenu) use ($children) {
+            foreach ($children as $child) {
+                $this->addSubItemToMenu($child, $subMenu);
+            }
+        });
+    }
+
+    /**
+     * Add children to the given menu recursively
+     * @param Menuitem $child
+     * @param PingpongMenuItem $sub
+     */
+    private function addSubItemToMenu(Menuitem $child, PingpongMenuItem $sub)
+    {
+        $sub->url($child->uri, $child->title);
+
+        if ($this->hasChildren($child)) {
+            $this->addChildrenToMenu($child->title, $child->items, $sub);
+        }
+    }
+
+    /**
+     * Check if the given menu item has children
+     *
+     * @param  object $item
+     * @return bool
+     */
+    private function hasChildren($item)
+    {
+        return $item->items->count() > 0;
     }
 }
