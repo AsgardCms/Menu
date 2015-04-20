fbfb7fc93e56e24e0ef8e019a0916148bcfc5555
Nicolas Widart <n.widart@gmail.com>
2015-04-20 09:05:58 +0200

    Update to latest pingpong/menus version

--- Providers/MenuServiceProvider.php
+++ Providers/MenuServiceProvider.php
@@ -8,8 +8,8 @@ use Modules\Menu\Repositories\Cache\CacheMenuDecorator;
 use Modules\Menu\Repositories\Cache\CacheMenuItemDecorator;
 use Modules\Menu\Repositories\Eloquent\EloquentMenuItemRepository;
 use Modules\Menu\Repositories\Eloquent\EloquentMenuRepository;
-use Pingpong\Menus\Builder;
-use Pingpong\Menus\Facades\Menu as MenuFacade;
+use Pingpong\Menus\MenuBuilder as Builder;
+use Pingpong\Menus\MenuFacade;
 use Pingpong\Menus\MenuItem as PingpongMenuItem;
 
 class MenuServiceProvider extends ServiceProvider
