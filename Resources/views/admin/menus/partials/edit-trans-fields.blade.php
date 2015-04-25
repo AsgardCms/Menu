377a18c4fb0680e59a40f6f1d70e190b769f664e
Nicolas Widart <n.widart@gmail.com>
2015-04-25 21:21:05 +0200

    Add hasTranslation check

--- Resources/views/admin/menus/partials/edit-trans-fields.blade.php
+++ Resources/views/admin/menus/partials/edit-trans-fields.blade.php
@@ -1,15 +1,17 @@
 <div class='form-group{{ $errors->has("{$lang}[title]") ? ' has-error' : '' }}'>
     {!! Form::label("{$lang}[title]", trans('menu::menu.form.title')) !!}
-    {!! Form::text("{$lang}[title]", Input::old("{$lang}[title]", $menu->translate($lang)->title), ['class' => 'form-control', 'placeholder' => trans('menu::menu.form.title')]) !!}
+    <?php $old = $menu->hasTranslation($lang) ? $menu->translate($lang)->title : '' ?>
+    {!! Form::text("{$lang}[title]", Input::old("{$lang}[title]", $old), ['class' => 'form-control', 'placeholder' => trans('menu::menu.form.title')]) !!}
     {!! $errors->first("{$lang}[title]", '<span class="help-block">:message</span>') !!}
 </div>
 <div class="checkbox">
+    <?php $old = $menu->hasTranslation($lang) ? $menu->translate($lang)->status : false ?>
     <label for="{{$lang}}[status]">
         <input id="{{$lang}}[status]"
                 name="{{$lang}}[status]"
                 type="checkbox"
                 class="flat-blue"
-                {{ ((bool)$menu->translate($lang)->status) ? 'checked' : '' }}
+                {{ ((bool) $old) ? 'checked' : '' }}
                 value="1" />
         {{ trans('menu::menu.form.status') }}
     </label>
