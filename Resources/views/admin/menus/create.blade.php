b07aeb2f492518fbeb749a27c0bf740d8bc260c8
Nicolas Widart <n.widart@gmail.com>
2015-04-28 21:09:25 +0200

    Adding keyboard shortcut

--- Resources/views/admin/menus/create.blade.php
+++ Resources/views/admin/menus/create.blade.php
@@ -64,9 +64,24 @@
 {!! Form::close() !!}
 @stop
 
+@section('footer')
+    <a data-toggle="modal" data-target="#keyboardShortcutsModal"><i class="fa fa-keyboard-o"></i></a> &nbsp;
+@stop
+@section('shortcuts')
+    <dl class="dl-horizontal">
+        <dt><code>b</code></dt>
+        <dd>{{ trans('core::core.back to index', ['name' => 'menu']) }}</dd>
+    </dl>
+@stop
+
 @section('scripts')
 <script>
 $( document ).ready(function() {
+    $(document).keypressAction({
+        actions: [
+            { key: 'b', route: "<?= route('admin.menu.menu.index') ?>" }
+        ]
+    });
     $('input[type="checkbox"].flat-blue, input[type="radio"].flat-blue').iCheck({
         checkboxClass: 'icheckbox_flat-blue',
         radioClass: 'iradio_flat-blue'
