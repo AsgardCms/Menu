34e970eee06d951ee0d4b7b96a6f95c48ef21d63
Nicolas Widart <n.widart@gmail.com>
2015-04-28 20:49:36 +0200

    Adding reset button

--- Providers/MenuServiceProvider.php
+++ Providers/MenuServiceProvider.php
@@ -153,6 +153,7 @@ class MenuServiceProvider extends ServiceProvider
         $menuItem = $this->app->make('Modules\Menu\Repositories\MenuItemRepository');
         foreach ($menu->allOnline() as $menu) {
             $menuTree = $menuItem->getTreeForMenu($menu->id);
+            //dd($menuTree);
             MenuFacade::create($menu->name, function (Builder $menu) use ($menuTree) {
                 foreach ($menuTree as $menuItem) {
                     $this->addItemToMenu($menuItem, $menu);
