d066c767da595a47266ca4003a9d61130ee1436c
franciscpd <franciscpd@gmail.com>
2015-07-19 17:03:23 -0400

    Adding portuguese brasilian locale

---
+++ Resources/lang/pt-BR/menu-items.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'form' => [
+        'page' => 'Página',
+        'module' => 'Módulo',
+        'target' => 'Objeto',
+        'same tab' => 'Mesma aba',
+        'new tab' => 'Nova aba',
+    ],
+];
---
+++ Resources/lang/pt-BR/messages.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    /* Menu management */
+    'menu created' => 'Menu criado com sucesso.',
+    'menu not found' => 'Menu não encontrado.',
+    'menu updated' => 'Menu atualizado com sucesso.',
+    'menu deleted' => 'Menu excluído com sucesso.',
+    /* MenuItem management */
+    'menuitem created' => 'Submenu criado com sucesso.',
+    'menuitem not found' => 'Submenu não encontrado.',
+    'menuitem updated' => 'Submenu atualizado com sucesso.',
+    'menuitem deleted' => 'Submenu excluído com sucesso.',
+];
