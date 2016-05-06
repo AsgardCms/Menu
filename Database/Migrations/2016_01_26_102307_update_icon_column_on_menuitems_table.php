0a070b84939b1593c7481c7b074ea85fa06cca32
optik <tomek@greymatter.pl>
2016-05-06 18:51:23 +0200

    Added migration to alter menu item icon field so that Pingpong MenuItem correctly renders empty icon (#26)

---
+++ Database/Migrations/2016_01_26_102307_update_icon_column_on_menuitems_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateIconColumnOnMenuitemsTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('menu__menuitems', function(Blueprint $table)
+        {
+            $table->string('icon')->nullable()->default(null)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('menu__menuitems', function(Blueprint $table)
+        {
+             $table->string('icon')->default('')->nullable(false)->change();
+        });
+    }
+
+}
