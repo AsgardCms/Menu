6237de32bce9f26c5729658f227f5e5a6ad9d640
Nicolas Widart <n.widart@gmail.com>
2016-06-29 22:24:50 +0200

    Removing Authorise middleware from api routes

--- Http/apiRoutes.php
+++ Http/apiRoutes.php
@@ -8,11 +8,9 @@ $router->group(['prefix' => '/menuitem'], function (Router $router) {
     $router->post('/update', [
         'as' => 'api.menuitem.update',
         'uses' => 'MenuItemController@update',
-        'middleware' => 'can:menu.menuitem.update',
     ]);
     $router->post('/delete', [
         'as' => 'api.menuitem.delete',
         'uses' => 'MenuItemController@delete',
-        'middleware' => 'can:menu.menuitem.destroy'
     ]);
 });
