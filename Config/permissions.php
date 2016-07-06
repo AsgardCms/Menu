933ec684f72f7b4379d5712f91e2207bf218388c
Nicolas Widart <n.widart@gmail.com>
2016-07-06 21:53:45 +0200

    Adding nicer permission labels

--- Config/permissions.php
+++ Config/permissions.php
@@ -2,15 +2,15 @@
 
 return [
     'menu.menus' => [
-        'index',
-        'create',
-        'edit',
-        'destroy',
+        'index' => trans('menu::menu.list resource'),
+        'create' => trans('menu::menu.create resource'),
+        'edit' => trans('menu::menu.edit resource'),
+        'destroy' => trans('menu::menu.destroy resource'),
     ],
     'menu.menuitems' => [
-        'index',
-        'create',
-        'edit',
-        'destroy',
+        'index' => trans('menu::menu-items.list resource'),
+        'create' => trans('menu::menu-items.create resource'),
+        'edit' => trans('menu::menu-items.edit resource'),
+        'destroy' => trans('menu::menu-items.destroy resource'),
     ],
 ];
