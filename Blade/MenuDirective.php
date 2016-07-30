9fe2cdd3cf8e130343b3fc5e4e9924197ba19954
Andrew Bekesh <arma@blackarma.com>
2016-07-30 23:41:26 +0300

    Finalazing
    -all changes
    
    Use correct check
    -Ignore browser check.
    
    Skeleton
    - Initial skeleton (room for improvement)

---
+++ Blade/MenuDirective.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Modules\Menu\Blade;
+
+final class MenuDirective
+{
+    private $name;
+    private $presenter;
+    private $bindings;
+
+    public function show($arguments)
+    {
+        $this->extractArguments($arguments);
+
+        return $this->returnMenu();
+    }
+
+    /**
+     * Extract the possible arguments as class properties
+     * @param array $arguments
+     */
+    private function extractArguments(array $arguments)
+    {
+        $this->name = array_get($arguments, 0);
+        $this->presenter = array_get($arguments, 1);
+        $this->bindings = array_get($arguments, 2, []);
+    }
+
+    /**
+     * Prepare arguments and return menu
+     * @return string|null
+     */
+    private function returnMenu()
+    {
+        if ($this->presenter === null) {
+            $this->presenter = config('asgard.menu.config.default_menu_presenter');
+        }
+
+        return app('menus')->get($this->name, $this->presenter, $this->bindings);
+    }
+
+    public function __toString()
+    {
+        return $this->returnMenu();
+    }
+}
