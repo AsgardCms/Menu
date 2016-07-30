2090c6591ec62e615d051eb1f1ec732a9a7b2269
Nicolas Widart <n.widart@gmail.com>
2016-07-30 22:54:31 +0200

    Alignment fix

--- Config/config.php
+++ Config/config.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-   /*
-   |--------------------------------------------------------------------------
-   | Custom Sidebar Class
-   |--------------------------------------------------------------------------
-   | If you want to customise the admin sidebar ordering or grouping
-   | You can define your own sidebar class for this module.
-   | No custom sidebar: null
-   */
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Sidebar Class
+    |--------------------------------------------------------------------------
+    | If you want to customise the admin sidebar ordering or grouping
+    | You can define your own sidebar class for this module.
+    | No custom sidebar: null
+    */
     'custom-sidebar' => null,
 
     /*
